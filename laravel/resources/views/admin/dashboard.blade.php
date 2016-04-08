92fb993eb8f23059d47a117166e47b14f571331f
zdettwiler <z.dettwiler@gmail.com>
2016-04-08 10:10:49 +0200

    Preliminary Dashboard

---
+++ laravel/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin_layout')
+
+
+@section('includes')
+    <link rel="stylesheet" media="screen" type="text/css" href="/css/main.css">
+@stop
+
+@section('main-content')
+
+	<h3>Articles</h3>
+	<h3>Événements</h3>
+	<h3>Équipe</h3>
+
+@stop
