9ae0ba58dd2f7de71bdfd47b950e000fa06cdd5e
zdettwiler <z.dettwiler@gmail.com>
2016-04-08 11:51:19 +0200

    Page Notre Expo Tournante text

--- laravel/resources/views/notre_expo_tournante.blade.php
+++ laravel/resources/views/notre_expo_tournante.blade.php
@@ -2,22 +2,22 @@
 
 @section('includes')
     {{-- <script src="js/layout.js"></script> --}}
-    <link rel="stylesheet" media="screen" type="text/css" href="/lautre-estaminet/public_html/css/main.css">
-    <link rel="stylesheet" media="screen" type="text/css" href="/lautre-estaminet/public_html/css/notre_expo_tournante.css">
+    <link rel="stylesheet" media="screen" type="text/css" href="/css/main.css">
+    <link rel="stylesheet" media="screen" type="text/css" href="/css/notre_expo_tournante.css">
 @stop
 
 @section('main-content')
 <div id="bg">
     <h1>NOTRE EXPO TOUR&shy;NANTE</h1>
 
-    <div id="events-notices">
+    <div id="exhibitors-notices">
         <h3>Actuellement à l'Autre Estaminet:</h3>
 
 
     </div>
 </div>
 
-    <div id="articles-container">
+    <div id="members-container">
 
     </div>
 
