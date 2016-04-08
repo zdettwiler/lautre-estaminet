b31c450b95b22ac92e1543983ab80a6e30c3b3fd
zdettwiler <z.dettwiler@gmail.com>
2016-04-08 11:50:52 +0200

    Page Nos Produits text

---
+++ laravel/resources/views/nos_produits.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.main_layout')
+
+@section('includes')
+    {{-- <script src="js/layout.js"></script> --}}
+    <link rel="stylesheet" media="screen" type="text/css" href="css/main.css">
+    <link rel="stylesheet" media="screen" type="text/css" href="css/nos_produits.css">
+@stop
+
+@section('main-content')
+	<div id="bg">
+	    <h1>NOS PRO&shy;DUITS</h1>
+	</div>
+
+	<div id="produits-text">
+
+		<h3>Jus de Fruits et Légumes &amp; Smoothies Frais</h3>
+		<p>
+			« Mangez au moins 5 fruits et légumes par jour » - objectif, pas toujours facile à atteindre, qui devient un jeu d'enfant pour ceux qui ont goûté à nos jus et smoothies.<br/>
+			Les aliments modernes nous placent devant un choix de consommation : bon goût ou bon pour la santé. Que choisir ?<br/>
+			Fini les choix cornéliens! Nos jus de fruits, nos légumes frais et nos smoothies vous offrent plaisir et santé, garantis sans concentrés ni sucres ajoutés avec, autant que possible, des produits du terroir. Tout est fait en direct, sur place devant vous, les jus devant être consommés rapidement pour ne pas perdre leurs qualités nutritives (vitamines) et leur saveur.<br/>
+			Faites-vous plaisir, venez faire le plein de vitamines! <br/><br/>
+
+
+		   <b>Smoothies</b>: <i>Barbe Rouge (Rhubarbe, Fraises, Poire, Pomme), Berry Good (Myrtilles, Fraises, Banane, Pomme, Orange, Lait), Bollywood (Mangue, Banane, Pomme, Orange, Gingembre, Citron vert), Corail (Mangue, Pomme, Orange, Fraises), Lait de Beauté (Poire, Amandes ou Noisette, Lait), Mishmash (Abricot Carotte, Pomme), Pacific (Mangue, Ananas, Lait de Coco, Pomme, Orange), Panthère Rose (Pomme, Orange, Framboises), Pom-Pom Fraise (Pommes, Fraises), Sunrise (Banane, Orange, Fraise), Sunset Boulevard (Framboises, Fraises, Pommes).</i></br></br>
+
+		   <b>Jus de Fruits</b>: <i>Jus d'Orange, Jus de Pomme, Antistress (Pomme, Orange, Citron), Beautiful (Pomme, Poire, Gingembre), Bien Vu (Carotte, Ananas, Orange), Boom (Bettrave rouge, Carotte, Pomme), Cassis Dream (Bettrave rouge, Cassis, Pomme), Light Juice (Carotte, Pomme, Orange), Pomme d'amour (Pomme, Gingembre), Purple Rain (Myrtille, Gingembre, Pamplemousse, Pomme), Spécial Estaminet (Poire, Orange, Pamplemousse), Un Goût de l'Est (Pomme, Bettrave Rouge).</i>
+		</p>
+
+		<h3>Cafés</h3>
+		<p>
+			Il existe environ 5 millions de plantations de café dans le monde. Les meilleures d'entre elles donnent un produit qui se consomme pur comme un grand cru. Ces cafés ont été primés lors de concours au fil des années. Nous proposons tous les jours un autre café pour faire un voyage aux quatre coins du monde. Cafés fruités ou floraux, avec des nuances d'épices, de miel, de caramel ou de chocolat. Longs en bouche, au corps plein et ayant une douceur naturelle ; un café différencié incite au rêve par ses arômes et saveurs uniques.</br>
+			Ces cafés ont reçu une attention particulière pendant toute la chaîne de production. Les cerises sont cueillies à la main, fermentées le jour même, lavées à l'eau de source, séchées au soleil et triées avec soin. Commerce équitable du producteur au consommateur.</br>
+			Ces cafés fins, grands cru, sont moulus directement dans le percolateur, avec le savoir-faire d'un barista et le meilleur équipement du marché afin de sauvegarder tout l'arôme du café. La preuve est dans la tasse!
+		</p>
+
+		<h3>Thés &amp; Tisanes</h3>
+		<p>
+			Toute la série des Thés Bien-être de Kusmi Tea.
+		</p>
+
+		<h3>Boissons Lactées</h3>
+		<p>
+			Nous utilisons le chocolat instantané suisse Caotina.
+		</p>
+
+		<h3>Boissons Froides</h3>
+		<p>
+			Diabolo, Gazolo, Vitello.
+			Nos sirops dont des sirops au sucre de canne : sans résidu chimique, sans additif, sans conservateur ni exhausteur de goût et sans colorants, qualité supérieure, pur fruit biologique.<br>
+
+			<i>Amaretto, Basilic, Caramel, Cassis, Cerise, Châtaigne, Citron, Fraise, Grenadine, Hibiscus, Kiwi, Lavande, Litchi, Menthe verte, Orange Sanguine, Pain d'épice, Pamplemousse, Pêche, Piñacolada, Spéculos, Violette.</i>
+		</p>
+
+		<h3>Soupes</h3>
+		<p>
+			Sans colorants ni conservateurs.<br>
+			<i>Velouté de potiron, Légumes du soleil, Légumes poëlés, Carottes fraîches, Poireaux/Pommes de terre, Potirons/Châtaignes, Pomme de terre/Poireaux/Lentilles, Mouliné de 7 légumes, Poisson.</i>
+		</p>
+
+		<h3>Crêpes &amp; Galettes</h3>
+		<p>
+			<b>Crêpes</b>: <i>Nature/Sucrée, Nutella, Spéculos, Snickers, Bountie, Sirop d'érable, Citronade, Confiture</i>.<br/>
+			<b>Galette Sarrasin</b>: <i>Jambon ou Blanc de poulet + 3Fromages ou Cr�me de Maroilles ou Crême de Saint Agur</i>
+		</p>
+
+		<h3>Glaces &amp; Sorbets</h3>
+		<p>
+			Un très large choix de parfums:<br>
+			<b>Glaces</b>: <i>café, praline-rose, bulgare-groseille, confiture de lait "fleur de sel de camargue", barbapapa, banane marbrée chocolat, vanille, praliné, lait de coco, pistache, stracciatella, chocolat...</i><br><br>
+			<b>Sorbets</b>: <i>passion, pomelos, framboise-violette, cactus, citron, mandarine de Sicile, pomme verte, melon, orange sanguine, fraise-basilic...</i><br><br>
+			Les glaces et sorbets que nous vous proposons sont élaborés dans une glacerie artisanale.<br>
+			Aucun arôme ni colorant n'est ajouté.<br>
+			Un effort de réduction des teneurs en sucre.
+		</p>
+
+		<p>
+			Ragusa, Amaretti, Toblerone
+		</p>
+	</div>
+
+@stop
